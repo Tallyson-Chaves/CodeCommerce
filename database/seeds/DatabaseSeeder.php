7f528fe4e603faba524c2cd56b34cacbe978f9ca
Tallyson Chaves <tallyson.adm@gmail.com>
2016-05-20 21:37:33 -0300

    Correcao Exercicio Seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+
+        $this->call('CategoriesSeeder');
+
+        $this->call('ProductsSeeder');
+
+        $this->call('UsersSeeder');
+
+
     }
 }
